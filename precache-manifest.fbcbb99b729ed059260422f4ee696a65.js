58a17adf49ac03be142e3629fb93349a92e8297a
Claire Filipek <cfilipek@sva.edu>
2020-05-03 10:13:13 -0400

    Updates

---
+++ precache-manifest.fbcbb99b729ed059260422f4ee696a65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93199a1e434192bc9076f2f126326504",
+    "url": "/why-not-try/index.html"
+  },
+  {
+    "revision": "d3ecf81116e9076dd717",
+    "url": "/why-not-try/static/css/main.bacb867c.chunk.css"
+  },
+  {
+    "revision": "c433f9d92321ba92cb8d",
+    "url": "/why-not-try/static/js/2.a641b77f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/why-not-try/static/js/2.a641b77f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ecf81116e9076dd717",
+    "url": "/why-not-try/static/js/main.01e3918b.chunk.js"
+  },
+  {
+    "revision": "0599620af2ce2dcde2f1",
+    "url": "/why-not-try/static/js/runtime-main.e208a4e5.js"
+  },
+  {
+    "revision": "f8c4dfd5fa4db49278bea7db4e53b86b",
+    "url": "/why-not-try/static/media/art-01.f8c4dfd5.png"
+  },
+  {
+    "revision": "950f724ec46bbad46df5a2333607a642",
+    "url": "/why-not-try/static/media/art-02.950f724e.png"
+  }
+]);
