bcb0f20f57f80c68b019f3a30b8f556c9af1dc37
Claire Filipek <cfilipek@sva.edu>
2020-05-04 08:21:45 -0400

    added worked on section and headers

--- src/Data.js
+++ src/Data.js
@@ -7,13 +7,13 @@ const data = [
     date: "mon may 04 2020",
     img: Art3,
     text: "I feel stuck. Stuck in the same routine; same pattern of being. I hope to breakout of this imbalance. I hope to quit smoking, I have a doctors appointment today to get the patch again, and start regularly exercising, and follow through with some tutorials. It feels like a lot but I am optimistic that I can manage despite quarantine and all else weighing me down.",
-    workedOn: ["to write"]
+    workedOn: ["-quitting smoking", "-birthday board game","-react tutorial"]
   },
   {
     date: "sun may 03 2020",
     img: Art2,
     text: "I am not sure what this is. Is it it a diary entry? Something for a public forum? How personal do I get? How deep will I go? For now these just seem like vague thoughts. A stream of consciousness that is very aware of its purpose to be be potentially public. It’s Sunday but it feels like any other day. Here I am writing this on my kitchen table. For me? For you?",
-    workedOn: ["to write"]
+    workedOn: ["-this website", "-my sanity","-went for a walk (moving more)"]
   },
   {
     date: "sat may 02 2020",
--- src/components/ShowEverything.js
+++ src/components/ShowEverything.js
@@ -11,7 +11,17 @@ const ShowEverything = () => {
             <div className="date">{item.date}</div>
             <div className="content">
               <img src={item.img} alt={`art-${i}`}/>
-              <h5 className="content-text">{item.text}</h5>
+              <div>
+                <h5 className="content-text"><b>Entry:</b></h5>
+                <h5 className="content-text">{item.text}</h5>
+                <h5 className="content-text"><b>Worked On:</b></h5>
+                {
+                item.workedOn.map((work, i) =>(
+                <div className="content-text" key={`${i}-workedon`}>
+                  {work}
+                </div>))
+                }
+              </div>
             </div>
           </div>
         ))
